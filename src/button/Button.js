f049472402899936dfb970d00ab7a749845806c8
J.Makavura <makavura@kosmic.co.za>
2023-06-07 09:12:03 +0300

    update: pass attributes

--- src/button/Button.js
+++ src/button/Button.js
@@ -1,7 +1,19 @@
+import combineClasses from "../utils/combineClasses";
 import Style from "./Button.module.css";
 
-const Button = ({ style = "", children = null }) => (
-  <button className={style == "primary" ? Style.PrimaryButton : Style.Button}>
+const Button = ({
+  style = "",
+  children = null,
+  className = "",
+  ...attributes
+}) => (
+  <button
+    className={combineClasses(
+      className,
+      style == "primary" ? Style.PrimaryButton : Style.Button
+    )}
+    {...attributes}
+  >
     {children}
   </button>
 );
